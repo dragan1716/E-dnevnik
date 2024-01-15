1dac7fde8f2c13ec716587d8db255486431befaa
Dragan <dragan.badnjar17@gmail.com>
2024-01-15 12:28:49 +0100

    dropdown-menu

---
+++ frontend/src/components/Menu/DropdownMenu.js
@@ -0,0 +1,29 @@
+import {
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Button,
+} from "@material-tailwind/react";
+import classes from "./DropdownMenu.module.css";
+import { IoSettingsOutline } from "react-icons/io5";
+
+export function DropdownMenu() {
+  return (
+    <Menu>
+      <MenuHandler>
+        <Button className={classes["btn"]}>
+          <div className={classes["name"]}>Dragan Badnjar</div>
+          <div className={classes["icon"]}>
+            <IoSettingsOutline />
+          </div>
+        </Button>
+      </MenuHandler>
+      <MenuList>
+        <MenuItem>Promjena lozinke</MenuItem>
+        <MenuItem>Menu Item 2</MenuItem>
+        <MenuItem>Odjava</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+}
