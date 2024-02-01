f8f68cf6520f5c08822bb0935f366ac4583ee23e
Dragan <dragan.badnjar17@gmail.com>
2024-02-01 09:51:24 +0100

    home-page

--- frontend/src/components/Home/HomePage.js
+++ frontend/src/components/Home/HomePage.js
@@ -2,20 +2,51 @@ import classes from "./HomePage.module.css";
 import Navbar from "../Bars/Navbar";
 import SideBar from "../Bars/SideBar";
 import FilterBar from "../Bars/FilterBar";
+import Card from "../Card/Card";
+import Timeline from "./Timeline";
+
 const HomePage = () => {
   return (
     <div className={classes["container"]}>
       <div className={classes["main-wrapper"]}>
         <div className={classes["nav-bar"]}>
-          <Navbar></Navbar>
+          <Navbar />
         </div>
         <div className={classes["side-bar"]}>
-          <SideBar></SideBar>
+          <SideBar />
         </div>
         <div className={classes["filter-bar"]}>
-          <FilterBar></FilterBar>
+          <FilterBar />
         </div>
-        <div className={classes["main-content"]}></div>
+        {/* <div className={classes["main-content"]}> */}
+        {/* <div className={classes["wrapper"]}>
+            <div className={classes["date"]}>
+              <div>01. 01. 2001</div>
+            </div>
+            <div className={classes["card"]}>
+              <Card />
+            </div>
+          </div> */}
+        {/* <div className={classes["wrapper"]}>
+            <div className={classes["date"]}>
+              <div>01. 01. 2001</div>
+            </div>
+            <div className={classes["card"]}>
+              <Card />
+            </div>
+          </div> */}
+        {/* <div className={classes["wrapper"]}> */}
+        {/* <div className={classes["date"]}>
+              <div>01. 01. 2001</div>
+            </div> */}
+        {/* <div className={classes["card"]}>
+              <Card />
+            </div> */}
+        {/* </div> */}
+        {/* </div> */}
+        <div>
+          <Timeline></Timeline>
+        </div>
       </div>
     </div>
   );
