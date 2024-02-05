bf93d7bae61f6c2afeac7c5ec9c89367c018ee4c
Dragan <dragan.badnjar17@gmail.com>
2024-02-05 09:13:51 +0100

    grade-service

--- backend/src/services/grade.service.js
+++ backend/src/services/grade.service.js
@@ -1,10 +1,27 @@
 const Grade = require('../models/grade.model');
-const ApiError = require('../utils/ApiError');
-const httpStatus = require('http-status');
 
 const createGrade = async (gradeBody) => {
   const grade = await Grade.create(gradeBody);
   console.log(grade);
 
   return grade;
+};
+
+/**
+ * Query for grades
+ * @param {Object} filter - Mongo filter
+ * @param {Object} options - Query options
+ * @param {string} [options.sortBy] - Sort option in the format: sortField:(desc|asc)
+ * @param {number} [options.limit] - Maximum number of results per page (default = 10)
+ * @param {number} [options.page] - Current page (default = 1)
+ * @returns {Promise<QueryResult>}
+ */
+const queryGrades = async (filter, options) => {
+  const grades = await Grade.paginate(filter, options);
+  return grades;
+};
+
+module.exports = {
+  createGrade,
+  queryGrades,
 };
