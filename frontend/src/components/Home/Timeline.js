665f2c3f93f8097186d2eb6865540463c3e9a3bc
Dragan <dragan.badnjar17@gmail.com>
2024-02-05 09:16:30 +0100

    timeline

--- frontend/src/components/Home/Timeline.js
+++ frontend/src/components/Home/Timeline.js
@@ -1,47 +1,45 @@
 import Card from "../Card/Card";
 import classes from "./Timeline.module.css";
+import { useState, useEffect } from "react";
 
 const Timeline = () => {
+  const [grades, setGrades] = useState([]);
+
+  const fetchGradesHandler = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/v1/grades");
+      const data = await response.json();
+      setGrades(data.results);
+
+      if (!response.ok) {
+        console.log("Fetching grades error");
+      }
+      console.log(data);
+      console.log(data.results[0]);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchGradesHandler();
+  }, []);
+
   return (
     <div className={classes["main-content"]}>
       <div>
         <div className={classes["events"]}>
-          <div className={classes["event"]}>
-            <div className={classes["event-timeline"]}>
-              {/*dodat sliku */}
-              <div className={classes["event-date"]}>03. 02. 2024</div>
+          {grades.map((grade) => (
+            <div className={classes["event"]} key={grade.id}>
+              <div className={classes["event-timeline"]}>
+                {/*dodat sliku */}
+                <div className={classes["event-date"]}>01. 02. 2024</div>
+              </div>
+              <div className={classes["event-data"]}>
+                <Card grade={grade} />
+              </div>
             </div>
-            <div className={classes["event-data"]}>
-              <Card></Card>
-            </div>
-          </div>
-          <div className={classes["event"]}>
-            <div className={classes["event-timeline"]}>
-              {/*dodat sliku */}
-              <div className={classes["event-date"]}>03. 02. 2024</div>
-            </div>
-            <div className={classes["event-data"]}>
-              <Card></Card>
-            </div>
-          </div>
-          <div className={classes["event"]}>
-            <div className={classes["event-timeline"]}>
-              {/*dodat sliku */}
-              <div className={classes["event-date"]}>03. 02. 2024</div>
-            </div>
-            <div className={classes["event-data"]}>
-              <Card></Card>
-            </div>
-          </div>
-          <div className={classes["event"]}>
-            <div className={classes["event-timeline"]}>
-              {/*dodat sliku */}
-              <div className={classes["event-date"]}>03. 02. 2024</div>
-            </div>
-            <div className={classes["event-data"]}>
-              <Card></Card>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
