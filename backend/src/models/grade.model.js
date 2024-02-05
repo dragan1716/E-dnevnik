dd9251595012df580cbd6f179424c5b5aeee9e22
Dragan <dragan.badnjar17@gmail.com>
2024-02-05 09:12:43 +0100

    grade-model

--- backend/src/models/grade.model.js
+++ backend/src/models/grade.model.js
@@ -1,23 +1,28 @@
 const mongoose = require('mongoose');
 const { toJSON, paginate } = require('./plugins');
 
-const gradeSchema = mongoose.Schema({
-  subjectName: {
-    type: String,
-    required: true,
-    trim: true,
+const gradeSchema = mongoose.Schema(
+  {
+    subjectName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    gradeText: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    gradeNumber: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
   },
-  gradeText: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  gradeNumber: {
-    type: Number,
-    required: true,
-    trim: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 // add plugin that converts mongoose to json
 gradeSchema.plugin(toJSON);
