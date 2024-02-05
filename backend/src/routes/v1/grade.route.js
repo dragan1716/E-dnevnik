817d1f049a04a207af533474696d7b4c666b7731
Dragan <dragan.badnjar17@gmail.com>
2024-02-05 09:12:20 +0100

    grade-route

---
+++ backend/src/routes/v1/grade.route.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const auth = require('../../middlewares/auth');
+const validate = require('../../middlewares/validate');
+const gradeValidation = require('../../validations/grade.validation');
+const gradeController = require('../../controllers/grade.controller');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .post(auth('manageGrades'), validate(gradeValidation.createGrade), gradeController.createGrade)
+  // .get(gradeController.getGrades);
+  .get(validate(gradeValidation.getGrades), gradeController.getGrades);
+
+// router
+//   .route('/:userId')
+//   .get(auth('getUsers'), validate(userValidation.getUser), userController.getUser)
+//   .patch(auth('manageUsers'), validate(userValidation.updateUser), userController.updateUser)
+//   .delete(auth('manageUsers'), validate(userValidation.deleteUser), userController.deleteUser);
+
+module.exports = router;
