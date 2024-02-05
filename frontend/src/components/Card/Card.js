1b70d832004cf0c680e30a877ca9a791698dbf77
Dragan <dragan.badnjar17@gmail.com>
2024-02-05 09:11:53 +0100

    card

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,17 +1,20 @@
 import classes from "./Card.module.css";
 
-const Card = () => {
+const Card = ({ grade }) => {
   return (
-    <div className={classes["container"]}>
-      <div className={classes["wrapper"]}>
-        <div className={classes["left"]}>
-          <div className={classes["grade"]}>5</div>
-        </div>
-        <div className={classes["right"]}>
-          <div className={classes["subject"]}>Matematika</div>
-          <div className={classes["final-grade"]}></div>{" "}
-          {/*Ovaj div treba da se prikaze samo kad je ocjena zakljucna*/}
-          <div className={classes["description"]}>Odlican (5)</div>
+    <div>
+      <div className={classes["container"]}>
+        <div className={classes["wrapper"]} key={grade.id}>
+          <div className={classes["left"]}>
+            <div className={classes["grade"]}>{grade.gradeNumber}</div>
+          </div>
+          <div className={classes["right"]}>
+            <div className={classes["subject"]}>{grade.subjectName}</div>
+            <div className={classes["final-grade"]}></div>{" "}
+            <div className={classes["description"]}>
+              {grade.gradeText} ({grade.gradeNumber})
+            </div>
+          </div>
         </div>
       </div>
     </div>
