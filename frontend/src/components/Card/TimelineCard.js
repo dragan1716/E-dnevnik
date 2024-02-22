1a384f4c35bb1ba665b4775b01dd26ffccb9e931
Dragan <dragan.badnjar17@gmail.com>
2024-02-22 10:55:01 +0100

    timeline-card

---
+++ frontend/src/components/Card/TimelineCard.js
@@ -0,0 +1,24 @@
+import classes from "./TimelineCard.module.css";
+
+const Card = ({ grade }) => {
+  return (
+    <div>
+      <div className={classes["container"]}>
+        <div className={classes["wrapper"]} key={grade.id}>
+          <div className={classes["left"]}>
+            <div className={classes["grade"]}>{grade.gradeNumber}</div>
+          </div>
+          <div className={classes["right"]}>
+            <div className={classes["subject"]}>{grade.subjectName}</div>
+            <div className={classes["final-grade"]}></div>{" "}
+            <div className={classes["description"]}>
+              {grade.gradeText} ({grade.gradeNumber})
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
