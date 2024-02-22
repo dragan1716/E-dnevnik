eb12eceebe8bb019d02cc0718fe9d20a3015e262
Dragan <dragan.badnjar17@gmail.com>
2024-02-22 10:55:41 +0100

    grade-api

--- frontend/src/libs/api/GradeApi.js
+++ frontend/src/libs/api/GradeApi.js
@@ -5,11 +5,9 @@ const fetchGradesHandler = async (page) => {
     const response = await axios.get(
       `http://localhost:3000/v1/grades?page=${page}` //&limit=${limit}`
     );
-    //console.log(response.data.results);
     console.log(response.data);
     console.log(response.data.results);
 
-    //return response.data.results;
     return response.data;
   } catch (error) {
     console.log(error.message);
