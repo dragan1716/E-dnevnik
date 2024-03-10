eed7f055166b031e4a2855567bcd04c3dafec3d0
Dragan <dragan.badnjar17@gmail.com>
2024-03-10 17:48:34 +0100

    app-frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,22 @@ import { Route, Routes } from "react-router-dom";
 import RegistrationPage from "./components/Registration/RegistrationPage";
 import LoginPage from "./components/Login/LoginPage";
 import HomePage from "./components/Home/HomePage";
+import GradeDetails from "./components/Home/Grades/GradeDetails";
+import Grades from "./components/Home/Grades/Grades";
+import Activities from "./components/Home/Activities/Activities";
+import Timeline from "./components/Home/Timeline/Timeline";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/registration" element={<RegistrationPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/subject/:id" element={<GradeDetails />} />
+        <Route path="/" element={<Timeline />} />
+        <Route path="/grades" element={<Grades />} />
+        <Route path="/activities" element={<Activities />} />
       </Routes>
     </>
   );
