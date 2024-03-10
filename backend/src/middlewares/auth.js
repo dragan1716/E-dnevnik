ccf414176f723811806168763082d7145dddd360
Dragan <dragan.badnjar17@gmail.com>
2024-03-10 17:33:41 +0100

    auth

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -4,7 +4,14 @@ const ApiError = require('../utils/ApiError');
 const { roleRights } = require('../config/roles');
 
 const verifyCallback = (req, resolve, reject, requiredRights) => async (err, user, info) => {
+  // Logiranje pristiglih podataka
+  console.log('Error:', err);
+  console.log('User:', user);
+  console.log('Info:', info);
+
   if (err || info || !user) {
+    // Logiranje greške ako se pojavi
+    console.error('Authentication error:', err || info || 'No user found');
     return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
   }
   req.user = user;
@@ -13,19 +20,23 @@ const verifyCallback = (req, resolve, reject, requiredRights) => async (err, use
     const userRights = roleRights.get(user.role);
     const hasRequiredRights = requiredRights.every((requiredRight) => userRights.includes(requiredRight));
     if (!hasRequiredRights && req.params.userId !== user.id) {
+      console.error('Forbidden:', 'User does not have required rights');
       return reject(new ApiError(httpStatus.FORBIDDEN, 'Forbidden'));
     }
   }
 
   resolve();
+  console.log('Authentication successful');
 };
 
-const auth = (...requiredRights) => async (req, res, next) => {
-  return new Promise((resolve, reject) => {
-    passport.authenticate('jwt', { session: false }, verifyCallback(req, resolve, reject, requiredRights))(req, res, next);
-  })
-    .then(() => next())
-    .catch((err) => next(err));
-};
+const auth =
+  (...requiredRights) =>
+  async (req, res, next) => {
+    return new Promise((resolve, reject) => {
+      passport.authenticate('jwt', { session: false }, verifyCallback(req, resolve, reject, requiredRights))(req, res, next);
+    })
+      .then(() => next())
+      .catch((err) => next(err));
+  };
 
 module.exports = auth;
