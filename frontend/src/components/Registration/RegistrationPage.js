3f2b8d882403e6f3394a361c07c8b37718e4b877
Dragan <dragan.badnjar17@gmail.com>
2024-03-10 17:40:14 +0100

    registration-page

--- frontend/src/components/Registration/RegistrationPage.js
+++ frontend/src/components/Registration/RegistrationPage.js
@@ -1,11 +1,21 @@
+import { useState, useEffect } from "react";
 import { RegistrationForm } from "./RegistrationForm";
 import classes from "./RegistrationPage.module.css";
+import { CustomSpinner } from "../Spinners/CustomSpinner";
 
 const RegistrationPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div className={classes.container}>
       <div className={classes.wrapper}>
-        <RegistrationForm />
+        {isLoading ? <CustomSpinner /> : <RegistrationForm />}
       </div>
     </div>
   );
