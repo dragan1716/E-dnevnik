3ecf00a5459a33978b496fe82c09d383cc1d2059
Dragan <dragan.badnjar17@gmail.com>
2024-03-10 17:34:55 +0100

    timeline-card

--- frontend/src/components/Card/TimelineCard.js
+++ frontend/src/components/Card/TimelineCard.js
@@ -1,18 +1,31 @@
 import classes from "./TimelineCard.module.css";
 
 const Card = ({ grade }) => {
+  const gradeValueText = (grade) => {
+    const gradeDescriptions = {
+      5: "Odlican",
+      4: "Vrlodobar",
+      3: "Dobar",
+      2: "Dovoljan",
+      1: "Nedovoljan",
+    };
+
+    return gradeDescriptions[grade];
+  };
+
   return (
     <div>
       <div className={classes["container"]}>
         <div className={classes["wrapper"]} key={grade.id}>
           <div className={classes["left"]}>
-            <div className={classes["grade"]}>{grade.gradeNumber}</div>
+            <div className={classes["grade"]}>{grade.value}</div>
           </div>
           <div className={classes["right"]}>
-            <div className={classes["subject"]}>{grade.subjectName}</div>
+            <div className={classes["subject"]}>{grade.subject}</div>
             <div className={classes["final-grade"]}></div>{" "}
             <div className={classes["description"]}>
-              {grade.gradeText} ({grade.gradeNumber})
+              {/*f-ja koja ispituje tekst(odlican, vrlodobar...), staticka helper metoda koja vraca vrijednost labele */}{" "}
+              {gradeValueText(grade.value)} ({grade.value})
             </div>
           </div>
         </div>
