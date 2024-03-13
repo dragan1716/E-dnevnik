e30b47f583319991d628b62e99320e0c3de979b1
Dragan <dragan.badnjar17@gmail.com>
2024-03-13 16:39:48 +0100

    subject-service

--- backend/src/services/subject.service.js
+++ backend/src/services/subject.service.js
@@ -1,10 +1,12 @@
 const Subject = require('../models/subject.model');
+const Semester = require('../models/semester.model');
 
 const createSubject = async (subjectBody) => {
   const subject = await Subject.create(subjectBody);
 
   return subject;
 };
+//dodati id za ocjenu, napraviti niz semestara
 
 /**
  * Query for subject
@@ -19,10 +21,86 @@ const createSubject = async (subjectBody) => {
  * @returns {Promise<Array>} - Array of grades for the specified subject
  */
 
-//arr grades,ime subjecta
+const assignGradesToSemesters = async (grades, semesters) => {
+  // const semesterGrades = semesters.map((semester) => ({
+  //   semesterType: semester.semesterType,
+  //   grades: [],
+  // }));
 
-const querySubjects = async (subjectName) => {
+  // grades.forEach((grade) => {
+  //   const { value, type, createdAt } = grade;
+  //   const newGrade = { value, type, createdAt };
+
+  //   semesters.forEach((semester) => {
+  //     const fromDate = new Date(semester.fromDate);
+  //     const toDate = new Date(semester.toDate);
+  //     const gradeDate = new Date(createdAt);
+
+  //     if (gradeDate >= fromDate && gradeDate <= toDate) {
+
+  //       if (semester.semesterType === 'winter') {
+  //         semesterGrades.grades.push(newGrade);
+  //       }
+  //       else if (semester.semesterType === 'summer') {
+  //         semesterGrades.grades.push(newGrade);
+  //       }
+  //     }
+  //   });
+  // });
+
+  // return semesterGrades;
   try {
+    // const semesterGrades = semesters.map((semester) => ({
+    //   semesterType: semester.semesterType,
+    //   grades: [],
+    // }));
+    const semesterGrades = semesters.map((semester) => ({
+      semesterType: semester.semesterType,
+      grades: [],
+    }));
+    console.log('SEMESTER GRADES: ', semesterGrades);
+
+    grades.forEach((grade) => {
+      const { _id, value, type, createdAt } = grade;
+      const newGrade = { _id, value, type, createdAt };
+
+      ////////////////////////////////////////////
+      // const gradeDate = new Date(grade.createdAt);
+
+      // semesterGrades.forEach((semester) => {
+      //   const fromDate = new Date(semester.fromDate);
+      //   const toDate = new Date(semester.toDate);
+      //   const gradeDate = new Date(createdAt);
+
+      // // if (gradeDate >= fromDate && gradeDate <= toDate) {
+      // semester.grades.push({
+      //   // _id: grade._id,
+      //   // value: grade.value,
+      //   // type: grade.type,
+      //   // createdAt: grade.createdAt,
+      //   newGrade,
+      // });
+      // }
+      semesters.forEach((semester, index) => {
+        const fromDate = new Date(semester.fromDate);
+        const toDate = new Date(semester.toDate);
+        const gradeDate = new Date(createdAt);
+
+        if (gradeDate >= fromDate && gradeDate <= toDate) {
+          semesterGrades[index].grades.push(newGrade);
+        }
+      });
+    });
+    console.log('SEMESTER GR: ', semesterGrades);
+    return semesterGrades;
+  } catch (error) {
+    throw new Error('Error assigning grades to semesters');
+  }
+};
+
+const querySubjects = async () => {
+  try {
+    const semesters = await Semester.find();
     const subjects = await Subject.aggregate([
       {
         $lookup: {
@@ -32,23 +110,98 @@ const querySubjects = async (subjectName) => {
           as: 'grades',
         },
       },
-      {
-        $project: {
-          subjectName: 1,
-          semesterId: '$grades.semesterId',
-          grades: '$grades.value',
-          type: '$grades.type',
-        },
-      },
+      // {
+      //   $project: {
+      //     subjectName: 1,
+      //     semesterId: '$grades.semesterId',
+      //     grades: '$grades.value',
+      //     type: '$grades.type',
+      //     createdAt: '$grades.createdAt',
+      //   },
+      // },
+      // {
+      //   $project: {
+      //     subjectName: 1,
+      //     grades: {
+      //       $map: {
+      //         input: semesters,
+      //         as: 'semester',
+      //         in: {
+      //           semesterNumber: '$$semester.semesterNumber',
+      //           grades: {
+      //             $filter: {
+      //               input: '$grades',
+      //               as: 'grade',
+      //               cond: {
+      //                 $and: [
+      //                   { $eq: ['$$grade.semesterId', '$$semester._id'] },
+      //                   { $gte: ['$$grade.createdAt', '$$semester.fromDate'] },
+      //                   { $lte: ['$$grade.createdAt', '$$semester.toDate'] },
+      //                 ],
+      //               },
+      //             },
+      //           },
+      //         },
+      //       },
+      //     },
+      //   },
+      // },
     ]);
-    console.log('Subjects:', subjects);
-    return subjects;
+
+    // const formattedSubjects = subjects.map((subject) => {
+    //   const gradesBySemester = assignGradesToSemesters(subject.grades, semesters);
+    //   console.log('GRADES BY SEMESTER', gradesBySemester);
+
+    //   return {
+    //     subjectName: subject.subjectName,
+    //     subjectId: subject._id,
+    //     semesters: gradesBySemester,
+    //   };
+    // });
+    const formattedSubjects = await Promise.all(
+      subjects.map(async (subject) => {
+        const gradesBySemester = await assignGradesToSemesters(subject.grades, semesters);
+        return {
+          subjectName: subject.subjectName,
+          subjectId: subject._id,
+          semesters: gradesBySemester,
+        };
+      })
+    );
+    console.log('Formatted Subjects:', formattedSubjects);
+    return formattedSubjects;
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //return subjects;
   } catch (error) {
     throw new Error('Error fetching subjects with grades');
+  }
+};
+
+/**
+ * Get subject by id
+ * @param {ObjectId} id
+ * @returns {Promise<Subject>}
+ */
+
+const getSubjectById = async (id) => {
+  try {
+    const subjects = await querySubjects();
+    const subject = subjects.find((subject) => subject._id.toString() === id.toString());
+
+    if (!subject) {
+      throw new Error('Subject not found');
+    }
+
+    return subject;
+  } catch (error) {
+    throw new Error('Error finding subject by ID');
   }
 };
 
 module.exports = {
   createSubject,
   querySubjects,
+  getSubjectById,
 };
