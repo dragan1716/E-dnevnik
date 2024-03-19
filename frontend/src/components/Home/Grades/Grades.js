eeb5fcc287045edc875daaefe085f5f3f52712f9
Dragan <dragan.badnjar17@gmail.com>
2024-03-19 10:09:27 +0100

    grades

--- frontend/src/components/Home/Grades/Grades.js
+++ frontend/src/components/Home/Grades/Grades.js
@@ -4,20 +4,22 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Layout from "../../layout/Layout";
+import { CustomSpinner } from "../../Spinners/CustomSpinner";
 
-//{onSubjectClick}
 const Grades = () => {
   const [subjectData, setSubjectData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const fetchGradesHandler = async () => {
     try {
+      console.log("Fetching grades...");
+
+      setIsLoading(true);
       const response = await axios.get(`http://localhost:3000/v1/subjects`);
       setSubjectData((prevSubjectData) => [
         ...prevSubjectData,
         ...response.data,
       ]);
-      setIsLoading(true);
       console.log(response);
       console.log(response.data);
 
@@ -29,19 +31,28 @@ const Grades = () => {
 
   useEffect(() => {
     fetchGradesHandler();
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timeout);
   }, []);
 
   return (
     <Layout>
       <div className={classes.container}>
         <div className={classes["items-wrap"]}>
-          {subjectData.map((subject) => (
-            <div className={classes["subjects-wrap"]} key={subject._id}>
-              <Link to={`/subject/${subject._id}`}>
-                <SubjectCard subject={subject} onFetch={fetchGradesHandler} />
-              </Link>
-            </div>
-          ))}
+          {isLoading ? (
+            <CustomSpinner />
+          ) : (
+            subjectData.map((subject) => (
+              <div className={classes["subjects-wrap"]} key={subject.subjectId}>
+                <Link to={`/subjects/${subject.subjectId}`}>
+                  <SubjectCard subject={subject} onFetch={fetchGradesHandler} />
+                </Link>
+              </div>
+            ))
+          )}
           <div className={classes["stats"]}></div>
         </div>
       </div>
