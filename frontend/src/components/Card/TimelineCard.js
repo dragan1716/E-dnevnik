de30be24d8db84431a1eb67c5a2042b59ad40774
Dragan <dragan.badnjar17@gmail.com>
2024-03-19 10:08:15 +0100

    timeline-card

--- frontend/src/components/Card/TimelineCard.js
+++ frontend/src/components/Card/TimelineCard.js
@@ -1,4 +1,7 @@
 import classes from "./TimelineCard.module.css";
+import { FaRegSmile } from "react-icons/fa";
+import { FaRegFaceMeh } from "react-icons/fa6";
+import { FaRegFaceFrown } from "react-icons/fa6";
 
 const Card = ({ grade }) => {
   const gradeValueText = (grade) => {
@@ -13,19 +16,52 @@ const Card = ({ grade }) => {
     return gradeDescriptions[grade];
   };
 
+  const renderIcon = () => {
+    if (grade.type === "activity") {
+      if (grade.value === "uspjesan") {
+        return <FaRegSmile className={classes["face-smile"]} />;
+      } else if (grade.value === "zadovoljava") {
+        return <FaRegFaceMeh className={classes["face-meh"]} />;
+      } else if (grade.value === "ne_zadovoljava") {
+        return <FaRegFaceFrown className={classes["face-frown"]} />;
+      }
+    }
+
+    return null;
+  };
+
+  const renderDescriptionText = () => {
+    if (grade.type === "activity") {
+      if (grade.value === "uspjesan") {
+        return "Uspješan";
+      } else if (grade.value === "zadovoljava") {
+        return "Zadovoljava";
+      } else if (grade.value === "ne_zadovoljava") {
+        return "Ne zadovoljava";
+      }
+    }
+
+    return `${gradeValueText(grade.value)} (${grade.value})`;
+  };
+
   return (
     <div>
       <div className={classes["container"]}>
         <div className={classes["wrapper"]} key={grade.id}>
           <div className={classes["left"]}>
-            <div className={classes["grade"]}>{grade.value}</div>
+            <div className={classes["grade"]}>
+              {renderIcon()}
+              {grade.type !== "activity" && `${grade.value}`}
+            </div>
           </div>
           <div className={classes["right"]}>
             <div className={classes["subject"]}>{grade.subject}</div>
             <div className={classes["final-grade"]}></div>{" "}
             <div className={classes["description"]}>
-              {/*f-ja koja ispituje tekst(odlican, vrlodobar...), staticka helper metoda koja vraca vrijednost labele */}{" "}
-              {gradeValueText(grade.value)} ({grade.value})
+              <div className={classes["description-grade"]}>
+                {renderDescriptionText()}
+              </div>
+              <div>{grade.description && <p>{grade.description}</p>}</div>
             </div>
           </div>
         </div>
