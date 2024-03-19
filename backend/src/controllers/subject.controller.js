d0464de542cdc922fa439580824a868b1c0440d2
Dragan <dragan.badnjar17@gmail.com>
2024-03-19 10:11:40 +0100

    subject-controller

--- backend/src/controllers/subject.controller.js
+++ backend/src/controllers/subject.controller.js
@@ -2,6 +2,7 @@ const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const { subjectService } = require('../services');
 const pick = require('../utils/pick');
+const ApiError = require('../utils/ApiError');
 
 const createSubject = catchAsync(async (req, res) => {
   const subject = await subjectService.createSubject(req.body);
@@ -9,14 +10,23 @@ const createSubject = catchAsync(async (req, res) => {
 });
 
 const getSubjects = catchAsync(async (req, res) => {
-  const filter = pick(req.query, ['subjectName', 'description']);
+  const filter = pick(req.query, ['subjectName']);
   const options = pick(req.query, ['sortBy', 'limit', 'page', 'populate']);
 
   const results = await subjectService.querySubjects(filter, options);
   res.send(results);
 });
 
+const getSubjectById = catchAsync(async (req, res) => {
+  const subject = await subjectService.getSubjectById(req.params.subjectId);
+  if (!subject) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Subject not found');
+  }
+  res.send(subject);
+});
+
 module.exports = {
   createSubject,
   getSubjects,
+  getSubjectById,
 };
